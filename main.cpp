d1a8d8545c6738a7a4b5d241e05668ac95b415d6
Christopher O'Toole <cdocq9@mst.edu>
2016-11-20 17:43:48 -0600

    Created config file and main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+   Programmers: Christopher O'Toole and Yunchao Zhang
+   Date: 
+   File: hw8_main.cpp
+   Purpose: 
+*/
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "town.h"
+#include "activist.h"
+#include "polluter.h"
+using namespace std;
+
+int main()
+{
+    return 0;
+}
